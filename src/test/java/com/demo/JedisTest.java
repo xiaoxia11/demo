00638faf41d79211f0137f0fa7a32630e44d5a72
xiaoxia11 <21631497@qq.com>
2018-09-29 09:14:21 +0800

    first commit

---
+++ src/test/java/com/demo/JedisTest.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+import org.junit.Test;
+
+import redis.clients.jedis.Jedis;
+
+public class JedisTest {
+	
+	@Test
+	public void test1() {
+		
+		Jedis jedis = new Jedis("127.0.0.1",6379);
+		String name = jedis.get("name");
+		System.out.println(name);
+		jedis.close();
+	}
+	
+}
