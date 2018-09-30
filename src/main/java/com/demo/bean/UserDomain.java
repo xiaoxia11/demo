ac3704ebc359d6afbd77b46b9c0542ce7da1ea3e
xiaoxia11 <21631497@qq.com>
2018-09-30 12:46:24 +0800

    热部署commit

--- src/main/java/com/demo/bean/UserDomain.java
+++ src/main/java/com/demo/bean/UserDomain.java
@@ -2,16 +2,24 @@ package com.demo.bean;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 //声明表名
 //@Table(name="person")
 public class UserDomain implements Serializable{
 	
 	private static final long serialVersionUID = -4577255781088498763L;
 	
+	//@JsonIgnore    //不显示
 	private String id;
 	private String name;
 	private String age;
 	private String sex;
+	//@JsonInclude(Include.NON_NULL)   //为空时不显示
+	//@JsonFormat(pattern="yyyy-MM-dd",locale="zh",timezone="GMT+8")   //Date装换字符串
 	private String txt;
 	
 	public String getId() {
